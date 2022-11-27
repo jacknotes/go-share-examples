4d36126b8fdf2e94de9bb24b3c95865698a0ce78
jack <jacknotes@163.com>
2022-11-27 22:27:44 +0800

    update cli

--- cloud-station/simple/main.go
+++ cloud-station/simple/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,8 +9,7 @@ import (
 )
 
 var (
-	conf     = NewDefaultConfig()
-	fileName = "go.mod"
+	conf = NewDefaultConfig()
 )
 
 type Config struct {
@@ -44,24 +44,65 @@ func LoadConfigFromEnv() {
 	conf.SK = os.Getenv("ALI_SK")
 }
 
-func UploadFile(filename string) error {
+func UploadFile(filename string) (downloadURL string, err error) {
 	// 连接OSS
 	client, err := oss.New(conf.Endpoint, conf.AK, conf.SK)
 	if err != nil {
-		return fmt.Errorf("new client error, %s\n", err)
+		err = fmt.Errorf("new client error, %s\n", err)
+		return
 	}
 	// 切换bucket
 	bucket, err := client.Bucket(conf.BucketName)
 	if err != nil {
-		return fmt.Errorf("get bucket %s error, %s\n", conf.BucketName, err)
+		err = fmt.Errorf("get bucket %s error, %s\n", conf.BucketName, err)
+		return
 	}
 	// 上传文件,arg1=oss存储的文件名称， arg2=原文件地址
 	err = bucket.PutObjectFromFile(filename, filename)
 	if err != nil {
-		return fmt.Errorf("upload file to bucket %s error, %s\n", conf.BucketName, err)
+		err = fmt.Errorf("upload file to bucket %s error, %s\n", conf.BucketName, err)
+		return
 	}
 
-	return nil
+	// 生成下载链接
+	return bucket.SignURL(filename, oss.HTTPGet, 60*60*24*3)
+}
+
+/*
+	CLI 说明
+*/
+
+var (
+	fileName string
+	help     bool
+)
+
+// 声明CLI的参数
+func init() {
+	// 声明cli参数
+	flag.StringVar(&fileName, "f", "", "请输入需要上传的文件路径")
+	flag.BoolVar(&help, "h", false, "打印本工具的使用说明")
+}
+
+// 工具使用说明函数
+func usage() {
+	fmt.Fprintf(os.Stderr, `cloud-station version: 0.0.1
+Usage: cloud-station [-h] -f <uplaod_file_path>
+Options:
+`)
+	// 打印默认参数
+	flag.PrintDefaults()
+}
+
+// 负责接收用户传入的参数
+func LoadArgsFromCLI() {
+	// 解析CLI参数，并赋值给变量
+	flag.Parse()
+
+	if help {
+		usage()
+		os.Exit(0)
+	}
 }
 
 // main是拼装流程的
@@ -75,13 +116,22 @@ func main() {
 		os.Exit(1)
 	}
 
+	// 接收用户参数
+	LoadArgsFromCLI()
+
 	// 上传文件
-	if err := UploadFile(fileName); err != nil {
+	downloadURL, err := UploadFile(fileName)
+	if err != nil {
 		fmt.Println(err)
 		os.Exit(2)
 	}
 
 	// 正常退出
-	fmt.Printf("文件: %s 上传成功", fileName)
+	fmt.Printf("文件: %s 上传成功\n", fileName)
+
+	// 打印下载链接
+	fmt.Printf("下载链接: %s\n", downloadURL)
+	fmt.Println("注意: 文件下载有效期为3天, 中转站保存时间为3天, 请及时下载")
+
 	os.Exit(0)
 }
